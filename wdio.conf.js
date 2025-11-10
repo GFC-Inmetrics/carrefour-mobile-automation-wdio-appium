9251a977b89f40d24614f9a651157f92204eb87b
guilherme.clemente <guilherme.clemente@inmetrics.com.br>
2025-11-10 09:59:24 -0300

    Projeto final com screenshot de falhas

--- wdio.conf.js
+++ wdio.conf.js
@@ -3,52 +3,73 @@ const deviceType = process.env.DEVICE || 'real';
 
 //  ANDROID DEVICE FÍSICO
 const androidRealDevice = {
-    platformName: 'Android',
-    'appium:deviceName': '192.168.0.89:5555',
-    'appium:automationName': 'UiAutomator2',
-    'appium:app': 'C:/Users/guilherme.clemente/android.wdio.native.app.v1.0.8.apk',
-    'appium:appPackage': 'com.wdiodemoapp',
-    'appium:appActivity': 'com.wdiodemoapp.MainActivity',
-    'appium:noReset': true,
-    'appium:autoGrantPermissions': true
+  platformName: 'Android',
+  'appium:deviceName': '192.168.0.89:5555',
+  'appium:automationName': 'UiAutomator2',
+  'appium:app': 'C:/Users/guilherme.clemente/android.wdio.native.app.v1.0.8.apk',
+  'appium:appPackage': 'com.wdiodemoapp',
+  'appium:appActivity': 'com.wdiodemoapp.MainActivity',
+  'appium:noReset': false,
+  'appium:autoGrantPermissions': true
 };
 
-//  ANDROID EMULADOR 
+//  ANDROID EMULADOR
 const androidEmulator = {
-    platformName: 'Android',
-    'appium:deviceName': 'Android Emulator',
-    'appium:automationName': 'UiAutomator2',
-    'appium:app': 'C:/Users/guilherme.clemente/android.wdio.native.app.v1.0.8.apk',
+  platformName: 'Android',
+  'appium:deviceName': 'Android Emulator',
+  'appium:automationName': 'UiAutomator2',
+  'appium:app': 'C:/Users/guilherme.clemente/android.wdio.native.app.v1.0.8.apk',
+  'appium:autoGrantPermissions': true,
+  'appium:noReset': false
 };
 
-//  iOS SIMULADOR 
+//  iOS SIMULADOR
 const iosSimulator = {
-    platformName: 'iOS',
-    'appium:deviceName': 'iPhone 14', // ou iPhone 13, 15, etc.
-    'appium:platformVersion': '16.4', // ajuste para a versão do Xcode instalada
-    'appium:automationName': 'XCUITest',
-    // OBS: o arquivo precisa ser `.app` gerado pelo Xcode para simulador
-    'appium:app': '/Users/seuUsuario/path/do/WdioDemoApp.app'
+  platformName: 'iOS',
+  'appium:deviceName': 'iPhone 14',
+  'appium:platformVersion': '16.4',
+  'appium:automationName': 'XCUITest',
+  'appium:app': '/Users/seuUsuario/path/WdioDemoApp.app'
 };
 
 function selectCapabilities() {
-    if (platform === 'android' && deviceType === 'real') return [androidRealDevice];
-    if (platform === 'android' && deviceType === 'emulator') return [androidEmulator];
-    if (platform === 'ios' && deviceType === 'emulator') return [iosSimulator];
+  if (platform === 'android' && deviceType === 'real') return [androidRealDevice];
+  if (platform === 'android' && deviceType === 'emulator') return [androidEmulator];
+  if (platform === 'ios' && deviceType === 'emulator') return [iosSimulator];
 }
 
 exports.config = {
-    runner: 'local',
-    specs: ['./test/specs/**/*.spec.js'],
-    maxInstances: 1,
-    capabilities: selectCapabilities(),
+  runner: 'local',
+  specs: ['./test/specs/**/*.spec.js'],
+  maxInstances: 1,
+  capabilities: selectCapabilities(),
 
-    logLevel: 'info',
-    framework: 'mocha',
-    mochaOpts: { timeout: 180000 },
+  logLevel: 'info',
+  hostname: '127.0.0.1',
+  port: 4723,
+  path: '/',
 
-    hostname: '127.0.0.1',
-    port: 4723,
-    path: '/',
-    services: []
+  framework: 'mocha',
+  mochaOpts: { timeout: 180000 },
+
+  reporters: [
+    'spec',
+    ['allure', {
+      outputDir: 'allure-results',
+      disableWebdriverStepsReporting: false,
+      disableWebdriverScreenshotsReporting: false,
+    }]
+  ],
+
+  //
+  // SCREENSHOT EM SUCESSO E FALHA
+  //
+  afterTest: async function(test, context, { error, result, duration, passed, retries }) {
+
+    if ( !passed) {
+    const screenshot = await browser.takeScreenshot();
+    const { addAttachment } = require('@wdio/allure-reporter').default;
+    addAttachment(`Screenshot - ${test.title}`, Buffer.from(screenshot, 'base64'), 'image/png');
+  }
+}
 };
